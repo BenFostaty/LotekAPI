49cdbfb6b128d24fed48fff661660ea3fb91c686
Benjamin Fostaty <126808545+BenFostaty@users.noreply.github.com>
2024-04-23 14:03:00 -0400

    Update Master.R

--- Master.R
+++ Master.R
@@ -6,8 +6,8 @@ setwd("ENTER LOCATION OF R SCRIPTS")
 source("packageInstaller.R") ## Checks package requirements,
                              ## manages installs and loads packages
 
-username <- "ENTER USERNAME"      ## Your Web Service username##
-password <- "ENTER PASSWORD"      ## Your Web Service password##
+username <- "ENTER USERNAME"      ## Your Web Service username as a character string##
+password <- "ENTER PASSWORD"      ## Your Web Service password as a character string##
 
 
 source("Login.R") ## Log in (Access Lasts 1 Hour)## 
@@ -18,7 +18,8 @@ source("refreshToken.R") ## Refresh the page (Token Lasts 1 week) ##
 
 source("deviceList.R")
 
-deviceID <- "152616" ## Applies to all single device functions
+deviceID <- "ENTER 5 OR 6 DIGIT DEVICE ID" ## Applies to all single device functions
+                                           ## Can be a character string or numeric
 
 source("allPositions.R") # May take a couple minutes to parse RxStatus. 
 
